6218b18830b62d0577f6c420bb4dee9b356df0e7
Olga Orel <olga.detterer@gmail.com>
2022-05-20 12:41:04 -0400

    Create, Update, delete TodoItem

--- cooperts-todo-app/app/controllers/todo_items_controller.rb
+++ cooperts-todo-app/app/controllers/todo_items_controller.rb
@@ -5,17 +5,23 @@ class TodoItemsController < ApplicationController
   end
 
   def create
-    todo_item = TodoItem.order("created_at DESC")
-    render json: todo_item
+    todo_item = TodoItem.new(title: params[:title], done: params[:done], user: User.last)
+    if todo_item.save
+      render json: todo_item
+    else
+      render json: todo_item.errors
+    end
   end
 
   def update
-    todo_item = TodoItem.order("created_at DESC")
+    todo_item = TodoItem.find(params[:id])
+    todo_item.update(done: params[:done])
     render json: todo_item
   end
 
   def destroy
-    todo_item = TodoItem.order("created_at DESC")
+    todo_item = TodoItem.find(params[:id])
+    todo_item.destroy
     render json: todo_item
   end
 end
